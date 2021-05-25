bef24c83daba470f9c074773d5529a457de68340
Leandro Tabares Martín <ltmartin@uci.cu>
2021-05-25 10:49:25 +0200

    Load replacements file feature tested and connection with Postgres database configured.

--- src/main/java/base/converters/MotivoToRdfConverter.java
+++ src/main/java/base/converters/MotivoToRdfConverter.java
@@ -3,9 +3,43 @@ package base.converters;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 @Component
 @Lazy
 public class MotivoToRdfConverter {
 
-    public void convert(){}
+    private ConcurrentMap<Integer, String> replacementsMap;
+
+    public MotivoToRdfConverter() {
+        replacementsMap = new ConcurrentHashMap();
+    }
+
+    public void convert(){
+        try {
+            loadReplacements();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadReplacements() throws FileNotFoundException {
+        File replacementsFile = new File("replacements.txt");
+        Scanner scanner = new Scanner(replacementsFile);
+        while (scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            int lastSpacePosition = line.lastIndexOf(" ");
+            String node = line.substring(0, lastSpacePosition);
+            Integer id = Integer.parseInt(line.substring(lastSpacePosition+1));
+            replacementsMap.put(id, node);
+        }
+    }
+
+    public ConcurrentMap<Integer, String> getReplacementsMap() {
+        return replacementsMap;
+    }
 }
